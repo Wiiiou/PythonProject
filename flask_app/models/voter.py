f570e20223b5a03da6c264e00d5e12469755557e
MedSouleim <147490372+MedSouleim@users.noreply.github.com>
2023-12-12 16:13:16 +0100

    commit

--- flask_app/models/voter.py
+++ flask_app/models/voter.py
@@ -16,18 +16,6 @@ class Voter:
         self.vote=data['vote']
         self.age=data['age']
         self.password=data['password']
-<<<<<<< HEAD
-        self.is_banned=data['is_banned']
-        self.first_time=data['first_time']
-        # self.new_password=data['new_password']
-        # self.confirm_password=data['confirm_password']
-    @classmethod
-    def create(cls,data):
-        query="""INSERT INTO voters
-        (first_name,last_name,email,password,birthdate,region,cin,vote,is_banned,first_time) VALUES
-                (%(first_name)s,%(last_name)s,%(email)s,%(password)s
-                ,%(birthdate)s,%(region)s,%(cin)s,0,0,1);"""
-=======
         self.is_banned = data['is_banned']
     @classmethod
     def create(cls,data):
@@ -35,7 +23,6 @@ class Voter:
         (first_name,last_name,email,password,age,region,cin,vote,is_banned) VALUES
                 (%(first_name)s,%(last_name)s,%(email)s,%(password)s,%(age)s
                 ,%(region)s,%(cin)s,0,0);"""
->>>>>>> 3c3bf16395b435605074b89d4d06bb470246df0d
         return connectToMySQL(database).query_db(query,data)
     
     @classmethod
@@ -54,18 +41,9 @@ class Voter:
         for row in db_result:
             all_voters.append(row)
         return all_voters
-    
     @classmethod
     def get_voter_by_email(cls,data):
         query="""SELECT * FROM voters WHERE email =%(email)s;"""
-        db_result=connectToMySQL(database).query_db(query,data)
-        if db_result:
-            return cls(db_result[0])
-        return None
-    
-    @classmethod
-    def get_voter_by_id(cls,data):
-        query="""SELECT * FROM voters WHERE id=%(id)s;"""
         db_result=connectToMySQL(database).query_db(query,data)
         if db_result:
             return cls(db_result[0])
@@ -75,14 +53,6 @@ class Voter:
         query="""
             UPDATE voters SET is_banned=%(x)s WHERE id=%(id)s;
             """ 
-        return connectToMySQL(database).query_db(query,data)
-    @classmethod
-    def update_first_time(cls,data):
-        query="""UPDATE voters SET first_time=%(first_time)s WHERE id=%(id)s;"""
-        return connectToMySQL(database).query_db(query,data)
-    @staticmethod
-    def update_first(data):
-        query="""UPDATE voters SET first_time=0,password=%(password)s WHERE id=%(id)s;"""
         return connectToMySQL(database).query_db(query,data)
     @staticmethod
     def validate(data):
@@ -99,9 +69,9 @@ class Voter:
         if data['region']=="Select your region":
             is_valid=False
             flash('please select your region')
-        if data['birthdate']=="":
+        if int(data['age'])<18:
             is_valid=False
-            flash('please insert your birthday')
+            flash("your age must be at leats 18")
         if len(data['password'])<8:
             for i in range(len(data['password'])):
                 if "A"<data['password'][i]<"Z":
@@ -122,18 +92,4 @@ class Voter:
                 flash('this Id already Exit')
                 print(is_valid)
                 break
-<<<<<<< HEAD
-        return is_valid
-    @staticmethod
-    def validate_new(data):
-        is_valid=True
-        if data['new_password']=="":
-            is_valid=False
-            flash('the password mustnt be empty please input the password')
-        if data['confirm_password']!=data['new_password']:
-            is_valid=False
-            flash('the confirmation is wrong pls fix that')
-        return is_valid
-=======
         return is_valid     
->>>>>>> 3c3bf16395b435605074b89d4d06bb470246df0d
